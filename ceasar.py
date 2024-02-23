32b9942d4ff1fd9c9308e2ffbea88b5df4c92ff2
Mahmoud Elshikha <133818711+medo3alaa@users.noreply.github.com>
2024-02-23 15:48:23 +0200

    ceasar.py

--- ceasar.py
+++ ceasar.py
@@ -1,40 +1,30 @@
-def caesar_cipher(data, shift, way):
-    """
-    Apply Caesar Cipher to the input data.
+import re
 
-    Parameters:
-    - data (str): The input text to be encrypted or decrypted.
-    - shift (int): The number of positions to shift the letters in the alphabet.
-    - way (str): 'e' for encryption, 'd' for decryption.
+def cc(data, shift, way):
+    r = ""
 
-    Returns:
-    - str: The result after applying Caesar Cipher to the input data.
-    """
-
-    result = ""
-
-    for char in data:
+    for c in data:
         # Check if the character is an alphabetical letter
-        if char.isalpha():
+        if c.isalpha():
             # Determine the ASCII offset based on the case (lower or upper)
-            ascii_offset = ord('a') if char.islower() else ord('A')
+            o = ord('a') if c.islower() else ord('A')
 
             # Calculate the new character after shifting
-            cipher_char = chr((ord(char) - ascii_offset + shift) % 26 + ascii_offset)
+            cipher_char = chr((ord(c) - o + shift) % 26 + o)
 
             # Append the encrypted or decrypted character based on the specified way
-            result += cipher_char if way == 'e' else chr((ord(char) - ascii_offset - shift) % 26 + ascii_offset)
+            r += cipher_char if way == 'e' else chr((ord(c) - o - shift) % 26 + o)
         else:
             # If the character is not an alphabetical letter, keep it unchanged
-            result += char
+            r += c
 
-    return result
+    return r
 
 
 # Get user input for the text, encryption/decryption, and shift value
-data = input("Please enter the text: ")
-way = input("Encrypt or decrypt the text? (e/d): ")
-shift = int(input("Please enter the shift value: "))
+d = input("Please enter the text: ")
+w = input("Encrypt or decrypt the text? (e/d): ")
+s = int(input("Please enter the shift value: "))
 
 # Print the result after encryption or decryption
-print(f"Text after {way}ion: {caesar_cipher(data, shift, way)}")
+print(f"Text after {w}ion: {cc(d, s, w)}")
