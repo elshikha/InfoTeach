557c9fa6655ae774e0535d87db0a82015cdffd3a
Mahmoud Elshikha <133818711+medo3alaa@users.noreply.github.com>
2024-02-23 15:47:24 +0200

    Pixel Manipulation.py

--- Pixel Manipulation.py
+++ Pixel Manipulation.py
@@ -1,45 +1,46 @@
+import re
+
 from PIL import Image
 
-def xor_cipher(input_data, encryption_key):
+def x_c(input_data, ek):
     """
     Apply XOR cipher to input data using the provided encryption key.
     """
-    return bytearray(a ^ encryption_key for a in input_data)
+    return bytearray(a ^ ek for a in input_data)
 
-def encrypt_image(input_path, encryption_key):
+def e_i(input_path, ek):
     """
     Encrypt an image using XOR cipher with the given encryption key.
     Save the encrypted image to a file named 'encrypted_image.png'.
     """
-    original_image = Image.open(input_path)
-    original_byte_data = bytearray(original_image.tobytes())
-    encrypted_data = xor_cipher(original_byte_data, encryption_key)
-    encrypted_image = Image.frombytes(original_image.mode, original_image.size, bytes(encrypted_data))
-    encrypted_image.save('encrypted_image.png')
+    o_i = Image.open(input_path)
+    o_b_d = bytearray(o_i.tobytes())
+    e_d = x_c(o_b_d, ek)
+    e_i = Image.frombytes(o_i.mode, o_i.size, bytes(e_d))
+    e_i.save('encrypted_image.png')
 
-def decrypt_image(input_path, decryption_key):
+def d_i(input_path, dk):
     """
     Decrypt an image using XOR cipher with the given decryption key.
     Save the decrypted image to a file named 'decrypted_image.jpg'.
     """
-    encrypted_image = Image.open(input_path)
-    encrypted_byte_data = bytearray(encrypted_image.tobytes())
-    decrypted_data = xor_cipher(encrypted_byte_data, decryption_key)
-    decrypted_image = Image.frombytes(encrypted_image.mode, encrypted_image.size, bytes(decrypted_data))
-    decrypted_image.save('decrypted_image.jpg')
-
+    e_i = Image.open(input_path)
+    e_b_d = bytearray(e_i.tobytes())
+    d_d = x_c(e_b_d, dk)
+    d_i = Image.frombytes(e_i.mode, e_i.size, bytes(d_d))
+    d_i.save('decrypted_image.jpg')
 
 # Get user input for the operation, file path, and secret key
-selected_operation = input("Do you want to encrypt or decrypt the image? Enter 'e' or 'd': ")
-image_file_path = input("Enter the file path of the image: ")
-secret_key = int(input("Enter the secret key (an integer from 0 to 255): "))
+s_o = input("Do you want to encrypt or decrypt the image? Enter 'e' or 'd': ")
+i_f_p = input("Enter the file path of the image: ")
+s_k = int(input("Enter the secret key (an integer from 0 to 255): "))
 
 # Perform the selected operation (encrypt or decrypt) on the image
-if selected_operation.lower() == 'e':
-    encrypt_image(image_file_path, secret_key)
+if s_o.lower() == 'e':
+    e_i(i_f_p, s_k)
     print("Image encrypted successfully!")
-elif selected_operation.lower() == 'd':
-    decrypt_image(image_file_path, secret_key)
+elif s_o.lower() == 'd':
+    d_i(i_f_p, s_k)
     print("Image decrypted successfully!")
 else:
     print("Invalid operation. Please enter 'encrypt' or 'decrypt'.")
